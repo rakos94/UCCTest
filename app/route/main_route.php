c5c2b864fb8781b622aa6335deb07d517b35d8bd
kamal <rakos94@gmail.com>
2020-10-09 19:48:47 +0700

    Add vehicle create function

--- app/route/main_route.php
+++ app/route/main_route.php
@@ -1,13 +1,17 @@
 <?php
 
 use App\Core\Router;
+use App\Models\EngineDisplacementType;
 use App\Models\Vehicle;
-use Jenssegers\Blade\Blade;
 
 /**
  * Create a new router instance.
  */
 $router = new Router($_SERVER);
+
+/**
+ * @var Jenssegers\Blade\Blade
+ */
 $blade = $app->blade;
 
 /**
@@ -15,11 +19,25 @@ $blade = $app->blade;
  */
 $router->addRoute('', function() use ($blade) {
     $vehicles = Vehicle::get();
-    echo $blade->make('index',['vehicles' => $vehicles])->render();
+    $type = EngineDisplacementType::get();
+    echo $blade->make('index',['vehicles' => $vehicles, 'type' => $type])->render();
 });
 
-$router->addRoute('/create', function() {
-    // require ROOT .'/views/index.php';
+$router->addRoute('create', function() {
+
+    $create = Vehicle::create([
+        'name'                  => $_REQUEST['name'],
+        'engine_displacement'   => $_REQUEST['engine_displacement'],
+        'engine_displacement_code'   => $_REQUEST['engine_displacement_code'],
+        'engine_power'          => $_REQUEST['engine_power'],
+        'price'                 => $_REQUEST['price'],
+        'location'              => $_REQUEST['location'],
+    ]);
+
+    // print($create->toJson());
+
+    if($create) header('Location: /');
+    
 });
 
 /**
